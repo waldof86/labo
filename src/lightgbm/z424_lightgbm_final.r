d7449c3135a1136076a1fb39fb6fc61e6f1158c1
Waldo Fattore <waldof86@gmail.com>
2023-04-19 19:13:34 +0200

    lightgbm homework

--- src/lightgbm/z424_lightgbm_final.r
+++ src/lightgbm/z424_lightgbm_final.r
@@ -20,13 +20,13 @@ PARAM$input$dataset       <- "./datasets/dataset_pequeno.csv"
 PARAM$input$training      <- c( 202107 )   #meses donde se entrena el modelo
 PARAM$input$future        <- c( 202109 )   #meses donde se aplica el modelo
 
-PARAM$finalmodel$semilla           <- 102191
+PARAM$finalmodel$semilla           <- 262139
 
-PARAM$finalmodel$num_iterations    <-  559
-PARAM$finalmodel$learning_rate     <-    0.0100746999
-PARAM$finalmodel$feature_fraction  <-    0.5144127527
-PARAM$finalmodel$min_data_in_leaf  <-    505
-PARAM$finalmodel$num_leaves        <-     44
+PARAM$finalmodel$num_iterations    <-  674
+PARAM$finalmodel$learning_rate     <-    0.0102732993313817
+PARAM$finalmodel$feature_fraction  <-    0.23708802573339
+PARAM$finalmodel$min_data_in_leaf  <-    607
+PARAM$finalmodel$num_leaves        <-     106
 
 
 PARAM$finalmodel$max_bin           <-     31
